842d17f92595a30247f389618a0cbf6912ba4a62
Glen Haber <GlenboLake@gmail.com>
2016-12-04 00:30:16 -0500

    Day 4 spacing

--- 2016/day4.py
+++ 2016/day4.py
@@ -4,6 +4,7 @@ frm = 'abcdefghijklmnopqrstuvwxyz-'
 to_ = 'bcdefghijklmnopqrstuvwxyza '
 table = str.maketrans(frm, to_)
 
+
 def check_room(room):
     match = re.match('([a-z\-]+)-(\d+)\[([a-z]{5})\]', room)
     name, id_, checksum = [match.group(x) for x in (1, 2, 3)]
@@ -11,10 +12,12 @@ def check_room(room):
     expected_checksum = ''.join(sorted(name_chars, key=lambda ch: name.count(ch), reverse=True)[:5])
     return int(id_) if checksum == expected_checksum else 0
 
+
 with open('day4.txt') as f:
     rooms = f.read().splitlines()
 
 print(sum(map(check_room, rooms)))
+
 
 def decrypt(room):
     match = re.match('([a-z\-]+)-(\d+)\[([a-z]{5})\]', room)
@@ -24,6 +27,7 @@ def decrypt(room):
         decrypted = decrypted.translate(table)
     if 'north' in decrypted:
         print(decrypted, id_)
+
 
 for room in rooms:
     decrypt(room)
