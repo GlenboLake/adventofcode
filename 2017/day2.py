5fda6e2921c5bde6cba2ca85376671d0e1ccffc6
Glen Haber <GlenboLake@gmail.com>
2017-12-02 00:13:21 -0500

    Day 2

---
+++ 2017/day2.py
@@ -0,0 +1,7 @@
+from itertools import permutations
+
+sheet = open('day2.in').read().splitlines()
+sheet = [list(map(int, row.split())) for row in sheet]
+
+print('Part 1:', sum(max(row) - min(row) for row in sheet))
+print('Part 2:', sum(a // b for r in sheet for a, b in permutations(r, 2) if a % b == 0))
