5ef0c4c7895af119ced59406cb0834fd1c60dfe1
Glen Haber <GlenboLake@gmail.com>
2017-12-17 00:37:07 -0500

    Day 17

---
+++ aoc2017/day17.py
@@ -0,0 +1,28 @@
+jump = 367
+
+
+def part1():
+    pos = 0
+    items = [0]
+    for i in range(1, 2018):
+        pos = (pos + jump) % len(items) + 1
+        items.insert(pos, i)
+    return items[pos + 1]
+
+
+def part2():
+    value = 0
+    pos = 0
+    for i in range(1, 50000000):
+        pos = (pos + jump) % i + 1
+        if pos == 1:
+            value = i
+    return value
+
+
+if __name__ == '__main__':
+    from time import time
+    start = time()
+    print('Part 1:', part1())
+    print('Part 2:', part2())
+    print(time()-start)
