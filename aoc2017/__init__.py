531e2172b85ad1c580d5dd69bac91a7778d4ebae
Glen Haber <glenbolake@gmail.com>
2017-12-18 13:00:48 -0500

    Optimize day 13

---
+++ aoc2017/__init__.py
@@ -0,0 +1,5 @@
+from math import gcd
+
+
+def lcm(a, b):
+    return a * b // gcd(a, b)
--- aoc2017/day13.py
+++ aoc2017/day13.py
@@ -1,16 +1,28 @@
-def calc_sev(delay):
-    severity = sum(k * v if (k + delay) % (2 * (v - 1)) == 0 else 0 for k, v in layers.items())
-    if severity == 0 and delay % (2 * (layers[0] - 1)) == 0:
-        return -1  # 0 severity, but still caught
+from functools import reduce
+
+from aoc2017 import lcm
+
+
+def calc_sev(layers):
+    severity = sum(k * v if k % (2 * (v - 1)) == 0 else 0 for k, v in layers.items())
     return severity
+
+
+def find_delay(layers):
+    cycle_length = reduce(lcm, (2 * (size - 1) for size in layers.values()))
+    for delay in range(cycle_length):
+        caught = False
+        for layer, depth in layers.items():
+            cycle = 2 * (depth - 1)
+            if (layer + delay) % cycle == 0:
+                caught = True
+                break
+        if not caught:
+            return delay
 
 
 if __name__ == '__main__':
     with open('day13.in') as f:
         layers = dict(map(int, line.split(': ')) for line in f)
-
-    print('Part 1:', calc_sev(0))
-    delay = 0
-    while calc_sev(delay):
-        delay += 1
-    print('Part 2:', delay)
+    print('Part 1:', calc_sev(layers))
+    print('Part 2:', find_delay(layers))
