396f324f6e46a2e4531607f84f8dfc6eb9b0e164
Glen Haber <GlenboLake@gmail.com>
2017-12-20 01:17:12 -0500

    Day 20

---
+++ aoc2017/day20.py
@@ -0,0 +1,42 @@
+import re
+from collections import namedtuple
+
+Particle = namedtuple('Particle', ['pos', 'vel', 'acc'])
+
+
+def parse_particle(line):
+    pos_match = re.search('p=<(-?\d+),(-?\d+),(-?\d+)>', line)
+    position = int(pos_match.group(1)), int(pos_match.group(2)), int(pos_match.group(3))
+    vel_match = re.search('v=<(-?\d+),(-?\d+),(-?\d+)>', line)
+    velocity = int(vel_match.group(1)), int(vel_match.group(2)), int(vel_match.group(3))
+    acc_match = re.search('a=<(-?\d+),(-?\d+),(-?\d+)>', line)
+    acceleration = int(acc_match.group(1)), int(acc_match.group(2)), int(acc_match.group(3))
+    return Particle(position, velocity, acceleration)
+
+
+def move_particle(particle):
+    new_v = tuple(v + a for v, a in zip(particle.vel, particle.acc))
+    new_p = tuple(p + v for p, v in zip(particle.pos, new_v))
+    return Particle(new_p, new_v, particle.acc)
+
+
+def manhattan(particle):
+    return sum(abs(k) for k in particle.pos)
+
+
+if __name__ == '__main__':
+    particles = [parse_particle(line) for line in open('day20.in')]
+
+    orig = particles.copy()
+
+    for _ in range(1000):
+        particles = [move_particle(p) for p in particles]
+    print(particles.index(min(particles, key=manhattan)))
+
+    particles = orig.copy()
+    for _ in range(1000):
+        if len(set(p.pos for p in particles)) < len(particles):
+            positions = [p.pos for p in particles]
+            particles = [part for part, pos in zip(particles, positions) if positions.count(pos) == 1]
+            print(len(particles))
+        particles = [move_particle(p) for p in particles]
